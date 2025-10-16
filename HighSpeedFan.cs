238fdef6fde489e5f4a9e5faa5d481f01e4f63f2
SkyLinepi <136251080+SkyLinepi@users.noreply.github.com>
2025-10-16 23:37:19 +0700

    SSSSSS

--- HighSpeedFan.cs
+++ HighSpeedFan.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 
 namespace Appliance
 {
-    internal class Fan : ISwitchControl
+    internal class Fan : ISwitchControl, ISpeedControl
     {
-        public float RotateSpeed = 190f;
+        private int CurrentSpeed = 0;
         bool _isOn;
         public bool IsOn
+        private int MaxS = 200;
+        private int MinS = 100;
         {
             get => _isOn;
 
@@ -18,13 +20,25 @@ namespace Appliance
 
         {
             _isOn = true;
-            Console.WriteLine("Fan is turning at a rate of "+ RotateSpeed);
+            CurrentSpeed = MinS;
+            Console.WriteLine("Fan is turning at a rate of "+ CurrentSpeed);
         }
 
         public void TurnOff()
         {
             _isOn = true;
+            CurrentSpeed = 0;
             Console.WriteLine("Fan gave up");
+        }
+
+        private void IncreaseS()
+        {
+            CurrentSpeed = MaxS;
+        }
+
+        public void DecreaseS()
+        {
+            CurrentSpeed = MinS;
         }
     }
 }
---
+++ ISpeedControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal interface ISpeedControl
+    {
+        int CurrentSpeed { get; }      
+        int MinSpeed { get; }     
+        int MaxSpeed { get; }   
+        void SetSpeed(int lv); 
+        void IncreaseSpeed();    
+        void DecreaseSpeed();  
+    }
+}
